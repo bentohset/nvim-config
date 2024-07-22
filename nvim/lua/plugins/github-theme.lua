0dbd629503cdfaa42e2359a5411f242891d8b44b
i749427 <benjamin.toh@sap.com>
2024-07-22 17:04:19 +0800

    fix colorscheme

--- nvim/lua/plugins/github-theme.lua
+++ nvim/lua/plugins/github-theme.lua
@@ -1,12 +1,32 @@
 return {
-  'projekt0n/github-nvim-theme',
+  "projekt0n/github-nvim-theme",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-  require("github-theme").setup({
-    -- ...
-  })
+    require("github-theme").setup({
+      specs = {
+        github_dark_dimmed = {
+          syntax = {
+            builtin1 = "orange",
+            constants = "orange",
+            type = "orange",
+            keywords = "orange",
+          }
+        }
+      },
+      groups = {
+        github_dark_dimmed = {
+          type = { fg = "orange" }
+        }
+      },
+      options = {
+        styles = {
+          comments = "italic",
+        },
+      },
+    })
+    
 
-  vim.cmd("colorscheme github_dark_dimmed")
+    vim.cmd("colorscheme github_dark_dimmed")
   end,
 }
