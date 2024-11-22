1a90afc6fe4b75fbdcd12ebaad57cbb0d34a35af
bentohset <toh.benjamin123@gmail.com>
2024-11-22 20:10:11 +0800

    add golang support

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,8 +16,12 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.coding" },
+    { import = "plugins.editor" },
+    { import = "plugins.ui" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
